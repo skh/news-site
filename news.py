c7ba32db589383b326844909bf77b21d569c5d49
Sonja Krause-Harder <skh@rcane.de>
2016-09-04 13:10:00 +0200

    Initial database connect working

--- news.py
+++ news.py
@@ -2,10 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/news"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2:///news"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# all queries we will ever need in one place
+queries={}
+queries['ARTICLES'] = "SELECT * FROM"
+
+
 db = SQLAlchemy(app)
+
+class Article(db.Model):
+	__tablename__ = 'articles'
+	id = db.Column(db.Integer, primary_key=True)
+	author = db.Column(db.Integer)
+	title = db.Column(db.String)
+	slug = db.Column(db.String, unique=True)
+	lead = db.Column(db.String)
+	body = db.Column(db.String)
+	time = db.Column(db.DateTime)
+
+
+@app.route('/')
+def main_page():
+	articles = Article.query.all()
+	print(articles)
+	return "<h1>Something is working</h1>"
 
 
 
