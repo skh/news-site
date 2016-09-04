9136192df740d1bcb13699112b8b56d76e190a50
Sonja Krause-Harder <skh@rcane.de>
2016-09-04 19:28:00 +0200

    Show article page and error page

--- news.py
+++ news.py
@@ -15,20 +15,40 @@ db = SQLAlchemy(app)
 class Article(db.Model):
 	__tablename__ = 'articles'
 	id = db.Column(db.Integer, primary_key=True)
-	author = db.Column(db.Integer)
-	title = db.Column(db.String)
-	slug = db.Column(db.String, unique=True)
+	author = db.Column(db.Integer, db.ForeignKey("authors.id"), nullable=False)
+	title = db.Column(db.String, nullable=False)
+	slug = db.Column(db.String, unique=True, nullable=False)
 	lead = db.Column(db.String)
 	body = db.Column(db.String)
 	time = db.Column(db.DateTime)
 
+class Author(db.Model):
+	__tablename__ = 'authors'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	bio = db.Column(db.String)
+	addresses = db.relationship('Article', backref='author_detail')
+
+
 
 @app.route('/')
 def main_page():
-	articles = Article.query.all()
-	print(articles)
+	articles = Article.query.order_by('time desc').limit(5).all()
 	return render_template("articles.html", articles=articles)
 
+@app.route('/article/<string:slug>')
+def article_page(slug):
+	article = Article.query.filter(Article.slug==slug).first()
+	if article:
+		return render_template("article.html", article=article)
+	else:
+		return render_template("404.html", slug=slug)
+
+def _log_success():
+	pass
+
+def log_error():
+	pass
 
 
 
