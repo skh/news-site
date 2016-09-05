1967324749f8e1ee6d6cad57e310ec6780691ee2
Sonja Krause-Harder <skh@rcane.de>
2016-09-05 09:33:30 +0200

    Log access, add new article form

--- news.py
+++ news.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from datetime import datetime
+
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects import postgresql
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2:///news"
@@ -29,28 +32,48 @@ class Author(db.Model):
 	bio = db.Column(db.String)
 	addresses = db.relationship('Article', backref='author_detail')
 
-
+class Log(db.Model):
+	__tablename__ = 'log'
+	id = db.Column(db.Integer, primary_key=True)
+	path = db.Column(db.String)
+	ip = db.Column(postgresql.INET)
+	method = db.Column(db.String)
+	status = db.Column(db.String)
+	time = db.Column(db.DateTime)
 
 @app.route('/')
 def main_page():
 	articles = Article.query.order_by('time desc').limit(5).all()
 	return render_template("articles.html", articles=articles)
 
+@app.route('/article/new', methods=['POST', 'GET'])
+def new_article():
+	if request.method == 'POST':
+		# title = request.form['title']
+		# slug = request.form['slug']
+		# lead = request.form['lead']
+		# body = request.form['body']
+		# print(title)
+		return "<h1>blah</h1>"
+	else:
+		return render_template("new_article.html")
+
 @app.route('/article/<string:slug>')
 def article_page(slug):
 	article = Article.query.filter(Article.slug==slug).first()
+	ip = request.remote_addr
 	if article:
+		_log_access('/article/' + slug, ip, "200 OK", 'GET')
 		return render_template("article.html", article=article)
 	else:
-		return render_template("404.html", slug=slug)
-
-def _log_success():
-	pass
-
-def log_error():
-	pass
+		_log_access('/article/' + slug, ip, "404 NOT FOUND", 'GET')
+		return render_template("404.html", slug=slug, ip=ip)
 
 
+def _log_access(path, ip, status, method):
+	log_entry = Log(path=path, ip=ip, status=status, method=method, time=datetime.now())
+	db.session.add(log_entry)
+	db.session.commit()
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
